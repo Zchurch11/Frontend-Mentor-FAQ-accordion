6bbb89d101a23b59b4189b2ba71eb4356614d285
Zchurch11 <157766777+Zchurch11@users.noreply.github.com>
2024-04-13 19:03:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const cards = document.querySelectorAll('.card')
-const btns = document.querySelectorAll('.btn-container')
+
 ;
 
 cards.forEach((card) => {
@@ -13,29 +13,3 @@ cards.forEach((card) => {
         
     })
 })
-// btns.forEach((btn) => {
-//     btn.addEventListener('click', () => {
-//         btn.parentNode.classList.toggle('active')
-//     })
-// })
-//     console.log(p.parentNode);)
-
-
-
-;
-// p.forEach((p) => {
-//     addEventListener( 'click', ()=> p.parentNode.classList.toggle('active'))
-//     console.log(p.parentNode);
-//})
-// function toggleActive(){
-//     cards.forEach((card) =>{
-//         if(card.classList.contains('active')){
-//             card.classList.remove('active')
-//         } else{
-//             card.classList.add('active')
-//         }
-//     })
-// }
-
-// cards.forEach((card) => addEventListener('click', toggleActive))
-// icons.addEventListener('click', activateIcons)
